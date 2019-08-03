ff17995f82366dd1c9aceb41d0d457868e38dbe4
paras madan <parasmadan15@yahoo.com>
2019-08-03 20:23:35 +0530

    emit newmessage on create new msg

--- server/server.js
+++ server/server.js
@@ -17,14 +17,15 @@ io.on('connection',(socket)=>{
     socket.on("disconnect",()=>{
         console.log("User was disconnected")
     })
-    socket.emit('newMessage',{
-        from: "John",
-        text: "See you then",
-        createAt:"123"
-    });
-
-    socket.on('createMessage',(newMessage)=>{
-        console.log('createMessage',newMessage);
+    
+    socket.on('createMessage',(message)=>{
+        console.log('createMessage',message);
+        
+        io.emit('newMessage',{
+            from:message.from,
+            text:message.text,
+            createdAt:new Date().getTime()
+        })
     })
 
 })
