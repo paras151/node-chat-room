5b8492b87b26f1a3a68acdc6843dafbfe11fbfd2
paras madan <parasmadan15@yahoo.com>
2019-08-04 14:39:09 +0530

    add button

--- public/js/index.js
+++ public/js/index.js
@@ -11,5 +11,28 @@ socket.on('disconnect',function(){
 });
 
 socket.on('newMessage',function(message){
-    console.log("New Message",message);
+    // console.log("New Message",message);
+
+    var li = jQuery('<li>');
+    li.text(`${message.from}: ${message.text}`)
+    jQuery('#messages').append(li);
+})
+
+// socket.emit('createMessage',{
+//     from:"Erik",
+//     text:"Erik is all fit and fine",
+//     createdAt:new Date().getTime()
+// },(s)=>{
+//     console.log("Got it "+s);
+// })
+
+jQuery('#message-form').on('submit',function(e){
+    e.preventDefault();
+
+    socket.emit('createMessage',{
+        from:'User',
+        text:jQuery('[name=message]').val()
+    },function(){
+
+    })
 })
