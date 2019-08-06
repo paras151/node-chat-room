3801c29c48878aa53740e51f49164e352f30ae89
paras madan <parasmadan15@yahoo.com>
2019-08-06 17:05:16 +0530

    adding scroll to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,21 @@
-
-
 var socket = io(); 
+
+function scrolltoBottom(){
+    //Selectors
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child')
+    //heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+
+    if(clientHeight + scrollTop +newMessageHeight + lastMessageHeight >= scrollHeight){
+        messages.scrollTop(scrollHeight)
+    }
+}
 
 socket.on('connect',function(){
     console.log("Connected to server");
@@ -27,6 +42,7 @@ socket.on('newMessage',function(message){
     });
 
     jQuery('#messages').append(html);
+    scrolltoBottom();
 })
 
 socket.on('newLocationMessage',function(message){
@@ -40,11 +56,6 @@ socket.on('newLocationMessage',function(message){
         createdAt:formattedTime
     });
     jQuery('#messages').append(html);
-
-    // li.text(`${message.from} ${formattedTime}: `);
-    // a.attr('href',message.url);
-    // li.append(a);
-    // jQuery('#messages').append(li);
 })
 
 
@@ -67,7 +78,7 @@ jQuery('#message-form').on('submit',function(e){
         
 
     })
-    // jQuery('[name=message]').val('');
+    jQuery('[name=message]').val('');
 
 })
 
